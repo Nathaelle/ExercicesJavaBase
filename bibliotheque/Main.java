a1de8ca25eb9095701f974f2f3657ddd89a04aa1
Nathaelle <scriptales.clients@gmail.com>
2021-06-01 14:23:51 +0200

    Exercice bibliotheque données mises à jour

--- bibliotheque/Main.java
+++ bibliotheque/Main.java
@@ -9,7 +9,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		initBibliotheque();
+		Main.initBibliotheque();
+		
+		
+		
 		
 		
 		
@@ -20,21 +23,26 @@ public class Main {
 		Livre livre1 = new Livre("rs548", "Les Misérables", "Victor Hugo");
 		Livre livre2 = new Livre("nb548",  "L'Iliade et l'Odyssée", "Homère");
 		Livre livre3 = new Livre("ut862", "1984", "George Orwell");
+		Livre livre4 = new Livre("fr693", "Voyage au centre de le terre", "Jules Vernes");
+		Livre livre5 = new Livre("sd861", "Ravage", "Barjavel");
+		Livre livre6 = new Livre("zq695", "Les fleurs du mal", "Baudelaire");
 		
 		System.out.println(livre1);
 		System.out.println(livre2);
 		System.out.println(livre3);
+		System.out.println(livre4);
+		System.out.println(livre5);
+		System.out.println(livre6);
 		
 		List<Livre> classiques = new ArrayList<>();
 		classiques.add(livre1);
 		classiques.add(livre2);
 		classiques.add(livre3);
+		classiques.add(livre4);
+		classiques.add(livre5);
+		classiques.add(livre6);
 		
-		Main.setBibliotheque(classiques);
-	}
-	
-	public static void setBibliotheque(List<Livre> bibli) {
-		bibliotheque = bibli;
+		bibliotheque = classiques;
 	}
 
 }
